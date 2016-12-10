233179ae2e76ca2ca8e8e2b657fbbdc60e38ca69
Silberklinge <git@github.com:Silberklinge/java-maze-solver.git>
2016-12-09 17:16:10 -0800

    Removed some clutter from the main method

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,35 +17,11 @@ import log_on.SploderClient;
 
 public class Main {
 	
-	public static int counter = -1;
-	
-	public static final Path DIRECTORY = Paths.get(System.getProperty("user.home"), "Desktop", "sploderwithinsploder");
-	public static final int GRAPHIC_SIZE = 60;
-	
 	public static void main(String[] args) throws Exception {
-		SploderClient c = new SploderClient("10000truths", "mai369iscool");
+		SploderClient c = new SploderClient("username", "password");
 		c.logIn();
-		BufferedImage smallThumb = ImageIO.read(DIRECTORY.resolve("smallThumb").toFile());
-		BufferedImage largeThumb = ImageIO.read(DIRECTORY.resolve("largeThumb").toFile());
-		int flags = PlatFlag.GRAPHICS | PlatFlag.COMMENTS;
-		PlatformerSettings settings = new PlatformerSettings("Sploder within Sploder", "10000truths", flags);
-		PlatformerXML xml = new PlatformerXML(settings);
 		
-		BufferedImage firstLevelImg = ImageIO.read(DIRECTORY.resolve("levels").resolve("level1").toFile());
-		
-		c.saveGame(false, smallThumb, largeThumb, null);
-	}
-	
-	public static Map<Integer, BufferedImage> partitionPicture(BufferedImage bi, int graphicNoCounter) {
-		Map<Integer, BufferedImage> imageMap = new HashMap<>();
-		for(int y = 0; y < bi.getHeight()/GRAPHIC_SIZE; y++)
-			for(int x = 0; x < bi.getWidth()/GRAPHIC_SIZE; x++) {
-				imageMap.put(graphicNoCounter--, bi.getSubimage(GRAPHIC_SIZE*x, GRAPHIC_SIZE*y, GRAPHIC_SIZE, GRAPHIC_SIZE));
-			}
-		return imageMap;
-	}
-	
-	public static void addImageToBackground(BufferedImage bi, int graphicNoCounter, int g) {
-		
+		// Replace null with an image object that satisfies the 20x20/40x40/60x60 precondition
+		c.saveGraphicAs(true, false, null);
 	}
 }
